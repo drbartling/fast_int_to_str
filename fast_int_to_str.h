12af3df162790f0986ad47b49f8e3524db5da11e
Ryan Bartling <ryan.bartling@gmail.com>
2016-05-06 17:01:28 -0700

    Update fast_int_to_str.h
    Added license to the header file.

--- fast_int_to_str.h
+++ fast_int_to_str.h
@@ -1,3 +1,40 @@
+/*******************************************************************************
+ * Fast Integer to String Conversion
+ *
+ * File Name:
+ *  fast_int_to_str.h
+ *
+ * Summary:
+ *  Provides API for fast integer to string conversion functions
+ *
+ * Description:
+ *  Provides API for fast integer to string conversion functions
+ *
+ ******************************************************************************/
+
+/*******************************************************************************
+MIT License
+
+Copyright (c) 2016 D. Ryan Bartling
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*******************************************************************************/
 
 #ifndef FAST_INT_TO_STR_H    // Guards against multiple inclusion
 #define FAST_INT_TO_STR_H
